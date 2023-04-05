e36856cdf61c2dd7e464a613d1e69ccdae4071b4
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-04-05 13:21:48 -0700

    Add and remove clicked class

--- javascript.js
+++ javascript.js
@@ -1,17 +1,6 @@
 const smallbox_div = document.getElementsByClassName(`innerBox`);
 const rpsButton_button = document.getElementById(`rpsButton`);
 
-smallbox_div[0].addEventListener('click', function(){
-    console.log(`clicked`)
-    activateBox(0);
-})
-
-smallbox_div[1].addEventListener('click', function(){
-    console.log(`clicked 1`)
-    activateBox(1);
-})
-
-
 
 
 
@@ -24,5 +13,43 @@ rpsButton_button.addEventListener(`click`, function(){
 
 
 function activateBox(i){
-    smallbox_div[i].classList.add(`hovered`);
+    smallbox_div[i].classList.add(`clicked`);
+}
+
+
+function deActivateBox(i){
+    smallbox_div[i].classList.remove(`clicked`);
+}
+
+
+
+
+smallbox_div[0].addEventListener('click', function(){
+    console.log(`clicked`)
+    if (smallbox_div[0].classList.contains(`clicked`)){
+        deActivateBox(0);
+    }
+    else {
+    activateBox(0);
+    }
+})
+
+smallbox_div[1].addEventListener('click', function(){
+    console.log(`clicked`)
+    if (smallbox_div[1].classList.contains(`clicked`)){
+        deActivateBox(1);
+    }
+    else {
+    activateBox(1);
+    }
+})
+/*      setTimeout(deActivateBox(1), 300);
+
+*/
+
+
+function clearBoxes(){
+    smallbox_div[0].classList.remove(`clicked`);
+    smallbox_div[1].classList.remove(`clicked`);
+
 }

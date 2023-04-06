f3ddc94027a159e1258d00da2c0e5c639708896f
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-04-06 09:09:22 -0700

    Make many boxes

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const smallbox_div = document.getElementsByClassName(`innerBox`);
 const rpsButton_button = document.getElementById(`rpsButton`);
-
+const gridBox_div = document.getElementById(`gridBox`);
 const clearButton_button = document.getElementById(`clearButton`);
 
 
@@ -8,21 +8,13 @@ const clearButton_button = document.getElementById(`clearButton`);
 rpsButton_button.addEventListener(`click`, function(){
     open(`https://kbouti.github.io/rock-paper-scissors/`);
 })
-
 clearButton_button.addEventListener(`click`, clearBoxes);
-
-
 function activateBox(i){
     smallbox_div[i].classList.add(`clicked`);
 }
-
-
 function deActivateBox(i){
     smallbox_div[i].classList.remove(`clicked`);
 }
-
-
-
 function clearBoxes(){
     for(let i = 0; i < 17; i++){
         deActivateBox(i);
@@ -31,25 +23,19 @@ function clearBoxes(){
 }
 
 
-function addListenerToBox(i){
-    smallbox_div[i].addEventListener('click', function(){
-        if (smallbox_div[i].classList.contains(`clicked`)){
-            deActivateBox(i);
-        }
-        else {
-        activateBox(i);
-        }
-    })
-}
 
-function applyAll() {
-for (let i = 0; i < 17; i++) {
-    addListenerToBox(i);
+
+function createInnerBox (){
+    const innerBox = document.createElement(`div`);
+    innerBox.classList.add(`innerBox`)
+    gridBox_div.appendChild(innerBox);
 
 }
+function makeBoxes(n){
+for (i = 0; i < n; i++){
+    createInnerBox();
+    console.log(`made a box`);
 }
-applyAll();
+}
 
-smallbox_div[4].addEventListener(`mouseenter`, function(){
-    console.log(`mouse enter`);
-})
+makeBoxes(256);

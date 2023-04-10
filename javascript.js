9578a074a50b13b5d4a0af25425c702f93dc822a
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-04-10 09:50:07 -0700

    Full-size grid with click on/off

--- javascript.js
+++ javascript.js
@@ -4,11 +4,58 @@ rpsButton_button.addEventListener(`click`, function(){
 })
 
 const gridBox_div = document.getElementById(`gridBox`);
+const smallBox_div = document.getElementsByClassName(`smallBox`);
 
-function createSmallBox(n){
-    let smallBox_div = document.createElement(`div`);
-    smallBox_div.classList.add(`smallBox`);
-    smallBox_div.id.add()
+
+//create an element
+//apply name
+//apply class
+//apply event listeners
+
+
+
+
+function makeElement(){
+    const newElement = document.createElement(`div`);
+    newElement.classList.add(`smallBox`);
+    gridBox_div.appendChild(newElement);
+}
+
+function makeElements(n){
+for (i=0; i <= n; i++){
+    makeElement()
+}
+}
+
+
+function onHover(i){
+    console.log(`hovered box ${i}`)
+    //smallBox_div[i]
+}
+
+
+
+
+
+makeElements(256);
+
+
+
+function addListenerToBox(n){
+    smallBox_div[n].addEventListener('click', function(){
+        if (smallBox_div[n].classList.contains(`clicked`)){
+            smallBox_div[n].classList.remove(`clicked`);
+        }
+        else {
+            smallBox_div[n].classList.add(`clicked`);
+        }
+    })
+}
+
+
+
+for (i = 0;i < smallBox_div.length; i++){
+    addListenerToBox(i);
 }
 
 
@@ -18,19 +65,45 @@ function createSmallBox(n){
 
 
 
-function createSmallBox (){
-    const smallBox_div = document.createElement(`div`);
-    smallBox_div.classList.add(`smallBox`)
-    gridBox_div.appendChild(smallBox_div);
+
+
+
+/*
+
+function createSmallBox(n){
+    const smallBox = document.createElement(`div`);
+    smallBox.classList.add(`smallBox`);
+    smallBox.setAttribute(`id`, `smallBox${n}`)
+    gridBox.appendChild(smallBox);
 
 }
 function makeBoxes(n){
     for (i = 0; i < n; i++){
-    createSmallBox();
+    createSmallBox(n);
     }
 }
 
 makeBoxes(256);
 
+function activateBox(n){
+smallBox_div[n].addEventListener('mouseenter', function(){
+    console.log(`hovered box ${n}`);
+    smallBox_div[n].classList.add(`hovered`);
+})
+smallBox_div[n].addEventListener('mouseleave', function(){
+    console.log(`left box ${n}`);
+    smallBox_div[n].classList.remove(`hovered`);
+})
+}
 
+activateBox(0);
 
+function activateAllBoxes(n){
+    for (i = 0; i <=n;i++){
+        activateBox(n);
+    }
+}
+
+activateAllBoxes(256);
+
+*/
